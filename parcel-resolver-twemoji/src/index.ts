2cb9199fa9b99f67d149223ed5274ea5d5d32e94
Tristan F <LeoDog896@hotmail.com>
2023-01-05 14:29:25 +0000

    Begin resolving emojis

--- parcel-resolver-twemoji/src/index.ts
+++ parcel-resolver-twemoji/src/index.ts
@@ -1,7 +1,34 @@
-import { resolver } from "@parcel/plugin";
+import { Resolver } from "@parcel/plugin";
+import path from 'path';
+import { Twemoji } from '@svgmoji/twemoji';
+import data from 'svgmoji/emoji.json';
+import fetch from "node-fetch"
 
-export default new resolver({
-  async transform({asset}) {
-    return [asset];
+const twemoji = new Twemoji({ data, type: 'all' });
+
+const emoji = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
+const emojiFile = /^[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}].svg$/u;
+
+export default new Resolver({
+  async resolve({specifier}) {
+    if (emojiFile.test(specifier)) {
+      const emojiCharacter = specifier.match(emoji)
+
+      if (!emojiCharacter || emojiCharacter.length === 0) {
+        throw Error("Character not recognized by regex.")
+      }
+
+      const request = await fetch(twemoji.url('❤️'))
+      const body = await request.text()
+
+      return {
+        filePath: path.join(__dirname, "emoji.svg"),
+        code: body
+      };
+    }
+
+    // Let the next resolver in the pipeline handle 
+    // this dependency.
+    return null;
   }
 });

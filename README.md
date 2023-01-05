e2c580935dc7b6e8dabde315e9684c37563a1c50
Tristan F <LeoDog896@hotmail.com>
2023-01-05 14:47:21 +0000

    fmt, working example

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ create images from your favorite emojis!
 
 ```html
 <!-- set your favicon as a burger -->
-<link rel="icon" type="image/x-icon" href="🍔.ico" sizes="any"/>
+<link rel="icon" type="image/x-icon" href="🍔.ico" sizes="any" />
 
 <!-- or maybe grab it as an image! -->
-<img src="🚀.png"/>
+<img src="🚀.png" />
 ```
 
 ```ts
 // even use it in URLs!
-const fishURL = new URL('🐟.svg', import.meta.url),
+const fishURL = new URL("🐟.svg", import.meta.url);
 ```
--- parcel-resolver-twemoji/src/index.ts
+++ parcel-resolver-twemoji/src/index.ts
@@ -1,34 +1,36 @@
 import { Resolver } from "@parcel/plugin";
-import path from 'path';
-import { Twemoji } from '@svgmoji/twemoji';
-import data from 'svgmoji/emoji.json';
-import fetch from "node-fetch"
+import path from "path";
+import { Twemoji } from "@svgmoji/twemoji";
+import data from "svgmoji/emoji.json";
+import fetch from "node-fetch";
 
-const twemoji = new Twemoji({ data, type: 'all' });
+const twemoji = new Twemoji({ data, type: "individual" });
 
-const emoji = /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
-const emojiFile = /^[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}].svg$/u;
+const emoji =
+  /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
+const emojiFile =
+  /^[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}].svg$/u;
 
 export default new Resolver({
-  async resolve({specifier}) {
+  async resolve({ specifier }) {
     if (emojiFile.test(specifier)) {
-      const emojiCharacter = specifier.match(emoji)
+      const emojiCharacter = specifier.match(emoji);
 
       if (!emojiCharacter || emojiCharacter.length === 0) {
-        throw Error("Character not recognized by regex.")
+        throw Error("Character not recognized by regex.");
       }
 
-      const request = await fetch(twemoji.url('❤️'))
-      const body = await request.text()
+      const request = await fetch(twemoji.url(emojiCharacter[0]));
+      const body = await request.text();
 
       return {
         filePath: path.join(__dirname, "emoji.svg"),
-        code: body
+        code: body,
       };
     }
 
-    // Let the next resolver in the pipeline handle 
+    // Let the next resolver in the pipeline handle
     // this dependency.
     return null;
-  }
+  },
 });

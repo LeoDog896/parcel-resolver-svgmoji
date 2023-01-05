d8311a5c71a0b80715af1dc7b1c437b5a861fe60
Tristan F <LeoDog896@hotmail.com>
2023-01-05 15:23:07 +0000

    Import all other -mojis

--- parcel-resolver-svgmoji/src/index.ts
+++ parcel-resolver-svgmoji/src/index.ts
@@ -1,10 +1,16 @@
 import { Resolver } from "@parcel/plugin";
 import path from "path";
 import { Twemoji } from "@svgmoji/twemoji";
+import { Blobmoji } from "@svgmoji/blob";
+import { Notomoji } from "@svgmoji/noto";
+import { Openmoji } from "@svgmoji/openmoji";
 import data from "svgmoji/emoji.json";
 import fetch from "node-fetch";
 
 const twemoji = new Twemoji({ data, type: "individual" });
+const blobmoji = new Blobmoji({ data, type: "individual" });
+const notomoji = new Notomoji({ data, type: "individual" });
+const openmoji = new Openmoji({ data, type: "individual" });
 
 const emoji =
   /[\p{Extended_Pictographic}\u{1F3FB}-\u{1F3FF}\u{1F9B0}-\u{1F9B3}]/u;
